d0579358b93ef35025bcc31d50e35548860294c5
Vlad20 <158486051+glemiu6@users.noreply.github.com>
2025-12-30 04:35:47 +0200

    menu for the avalible options

--- cli/menu.py
+++ cli/menu.py
@@ -1,14 +1,18 @@
-def show_menu()->dict[int,str]:
-    menu={
+def show_menu():
+    options = {
         1:"Scrape books",
         2:"List all books",
         3:"Filter books",
         4:"Create User",
         5:"Assign book to user",
-        6:"View user's books"
+        6:"View user's books",
+        0:"Exit"
     }
 
-    for k,v in menu.items():
+    for k,v in options.items():
         print(f"{k}. {v}")
 
-    return menu
+
+
+if __name__=="__main__":
+    menu=show_menu()
